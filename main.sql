c5d06e6fd2003d80163b465d60d566b439262f1a
Ramses <39100042+charlietud@users.noreply.github.com>
2023-01-24 04:10:34 +0000

    Added a section to explain the WHERE keyword and the order of execution

--- main.sql
+++ main.sql
@@ -39,3 +39,22 @@ SELECT COUNT(*)
 -- Using DISTINCT along with count to get the number of unique values
 SELECT COUNT(DISTINCT name) AS distinct_names
   FROM table_name;
+
+-- Comparison operators
+-- =, !=, <> (not equal to), <, >, <=, >=, BETWEEN, IN, LIKE, NOT LIKE, IS NULL, IS NOT NULL
+
+-- Select all columns where the name is "John"
+SELECT *
+  FROM table_name
+  WHERE name = 'John'; --Make sure to use single quotes for strings and double quotes for column names
+
+-- Order of execution
+-- 1. FROM
+-- 2. WHERE
+-- 3. GROUP BY
+-- 4. HAVING
+-- 5. SELECT
+-- 6. DISTINCT
+-- 7. ORDER BY
+-- 8. LIMIT
+
