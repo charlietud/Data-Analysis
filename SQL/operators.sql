8e1380513d06dcba5a3037b4df032634d1edb696
Ramses <39100042+charlietud@users.noreply.github.com>
2023-01-24 04:36:31 +0000

    Created a new file to organize the SQL info a bit better

--- SQL/operators.sql
+++ SQL/operators.sql
@@ -1,45 +1,3 @@
-PosgreSQL
--- Basic queries
-
--- 1. Select all columns from the table
-SELECT * 
-  FROM table_name;
-
--- 2. Select only the name column
-SELECT name
-  FROM table_name;
-
--- Use DISTINCT to only return unique values
-SELECT DISTINCT name
-           FROM table_name;
-
--- Adding an alias to a column
-SELECT DISTINCT name AS "Name"
-           FROM table_name;
-
--- Create a view of the query results
-    CREATE VIEW view_name AS 
-SELECT DISTINCT name AS "Name"
-           FROM table_name;
-
--- Limit the number of rows returned
-SELECT DISTINCT name AS "Name"
-           FROM table_name
-           LIMIT 5;
-
---INTERMEDIATE QUERIES
-
--- Use COUNT to count the number of rows (number of records/rows in a field/column)
-SELECT COUNT(name)
-  FROM table_name;
--- Get all values
-SELECT COUNT(*)
-  FROM table_name;
-
--- Using DISTINCT along with count to get the number of unique values
-SELECT COUNT(DISTINCT name) AS distinct_names
-  FROM table_name;
-
 -- Comparison operators
 -- =, !=, <> (not equal to), <, >, <=, >=, BETWEEN, IN, LIKE, NOT LIKE, IS NULL, IS NOT NULL
 
@@ -90,3 +48,4 @@ FROM films
 WHERE (release_year = 1990 OR release_year = 1999)
 AND (language = 'English' OR language = 'Spanish')
 AND gross > 2000000;
+
