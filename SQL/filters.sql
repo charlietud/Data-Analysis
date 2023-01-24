ec45f5cdbea9a1f63a46c811b318099ff8c6071c
Ramses <39100042+charlietud@users.noreply.github.com>
2023-01-24 19:57:13 +0000

    Created a new file about filters

---
+++ SQL/filters.sql
@@ -0,0 +1,28 @@
+-- Filtering with LIKE and NOT LIKE
+
+--Select names that start with "B"
+SELECT name
+  FROM table_name
+  WHERE name LIKE 'B%';
+
+--Select names that have an "r" in the second position
+SELECT name
+  FROM table_name
+  WHERE name LIKE '_r%';
+
+--Select names that dont start with an "A"
+SELECT name
+  FROM table_name
+  WHERE name NOT LIKE 'A%';
+
+-- WHERE IN
+-- Find the title, certification, and language all films certified NC-17 or R that are in English, Italian, or Greek
+SELECT title, certification, language
+  FROM films
+  WHERE certification IN ('NC-17', 'R')
+  AND language IN ('English', 'Italian', 'Greek');
+
+
+-- So far our SQL vocabulary includes COUNT(), DISTINCT, LIMIT, WHERE, OR, AND, BETWEEN, LIKE, NOT LIKE, and IN.
+
+
