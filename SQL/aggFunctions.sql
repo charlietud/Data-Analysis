0061613b5d08738d6d4eec396b9020b280c13581
Ramses <39100042+charlietud@users.noreply.github.com>
2023-01-26 19:45:05 +0000

    Added the ROUND function

--- SQL/aggFunctions.sql
+++ SQL/aggFunctions.sql
@@ -16,3 +16,10 @@ SELECT MIN(release_year) AS earliest_release_year
     FROM films;
 
 -- Summarizing subsets
+-- Using ROUND() to round the average duration to the nearest whole number
+SELECT ROUND(AVG(duration), 2) AS average_duration --The second parameter indicates the number of decimal places to round to
+    FROM films;
+
+-- Ej.Calculate the average budget rounded to the thousands (using a negative number for the second parameter)
+SELECT ROUND(AVG(budget), -3) AS average_budget
+    FROM films;
